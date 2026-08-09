f1f559c86996cbc70d93b4a014e0bfe08b5a42ee
Amaya <amayasenavirathne001@gmail.com>
2026-08-09 20:28:51 +0530

    Initial EcoMate project setup

---
+++ backend/src/test/java/com/ecomate/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomate.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
